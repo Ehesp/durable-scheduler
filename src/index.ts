b2ea16e12a7805de035ea3e7bc9be0b88c8e98ab
Elliot Hesp <elliot.hesp@gmail.com>
2025-05-09 14:21:57 +0100

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,51 @@
 import { DurableObject } from "cloudflare:workers";
 import { drizzle, DrizzleSqliteDODatabase } from "drizzle-orm/durable-sqlite";
 import { CronExpressionParser } from "cron-parser";
-import { migrate } from 'drizzle-orm/durable-sqlite/migrator';
+import { migrate } from "drizzle-orm/durable-sqlite/migrator";
 import { eq, and, gt, asc, ne, lte, count } from "drizzle-orm";
 import { type Task, tasks } from "./db";
 
 import migrations from "./migrations/migrations";
 
-type Env = {};
-
 export type BaseSchedule = {
   // The id of the schedule, defaults to a random UUID.
   id?: string;
   // The description of the schedule.
-  description: Task['description'];
+  description: Task["description"];
   // The payload of the schedule.
   payload?: Record<string, unknown>;
 };
 
 export type CronSchedule = BaseSchedule & {
   // A cron schedule.
-  type: 'cron';
+  type: "cron";
   // The cron expression to schedule the task.
   cron: string;
 };
 
 export type DelayedSchedule = BaseSchedule & {
   // A delayed schedule.
-  type: 'delayed';
+  type: "delayed";
   // The number of seconds to delay the task.
   seconds: number;
 };
 
 export type ScheduledSchedule = BaseSchedule & {
   // A datetime schedule.
-  type: 'scheduled';
+  type: "scheduled";
   // The datetime to schedule the task.
   date: Date;
 };
 
 export type NoSchedule = BaseSchedule & {
-  type: 'no-schedule';
+  type: "no-schedule";
 };
 
-export type Schedule = CronSchedule | DelayedSchedule | ScheduledSchedule | NoSchedule;
+export type Schedule =
+  | CronSchedule
+  | DelayedSchedule
+  | ScheduledSchedule
+  | NoSchedule;
 
 export type QueryOptions = {
   // The limit of the number of tasks to return, defaults to 100.
@@ -51,9 +53,9 @@ export type QueryOptions = {
   // The offset of the tasks to return, defaults to 0.
   offset?: number;
   // The order of the tasks to return by date, defaults to descending.
-  orderBy?: 'asc' | 'desc';
+  orderBy?: "asc" | "desc";
   // The type of the tasks to return, defaults to all types.
-  type?: Task['type'];
+  type?: Task["type"];
 };
 
 // The schema of the DO database for Drizzle.
@@ -71,9 +73,9 @@ export abstract class DurableScheduler<Env> extends DurableObject<Env> {
     this.db = drizzle(this.storage, { logger: false, schema });
 
     state.blockConcurrencyWhile(async () => {
-			await this.migrate();
+      await this.migrate();
       await this.alarm();
-		});
+    });
   }
 
   // Returns the status of the scheduler.
@@ -94,17 +96,24 @@ export abstract class DurableScheduler<Env> extends DurableObject<Env> {
   // Schedules the next alarm.
   private async scheduleNextAlarm() {
     // Get the next task to execute.
-    const [nextTask] = await this.db.select({
-      date: tasks.date,
-    }).from(tasks).where(
-      and(
-        gt(tasks.date, new Date()),
-        ne(tasks.type, 'no-schedule')
-      )
-    ).orderBy(asc(tasks.date)).limit(1);
-    
-    if (!nextTask || !nextTask.date) return;
-    await this.ctx.storage.setAlarm(nextTask.date);  
+    const [nextTask] = await this.db
+      .select({
+        date: tasks.date,
+      })
+      .from(tasks)
+      .where(and(gt(tasks.date, new Date()), ne(tasks.type, "no-schedule")))
+      .orderBy(asc(tasks.date))
+      .limit(1);
+
+    const nextDate = nextTask?.date;
+
+    if (!nextDate) {
+      console.log("No next task, no alarm scheduled");
+      return;
+    }
+
+    console.log(`Scheduling alarm for ${nextDate.toLocaleString()}`);
+    await this.ctx.storage.setAlarm(nextDate);
   }
 
   // Returns the next cron time for the given cron expression.
@@ -115,42 +124,60 @@ export abstract class DurableScheduler<Env> extends DurableObject<Env> {
 
   // Executes all expired tasks which need to be executed.
   async alarm(): Promise<void> {
+    console.log("Executing alarm");
+
     // Get all expired tasks which need to be executed.
-    const expired = await this.db.select().from(tasks).where(
-      and(
-        lte(tasks.date, new Date()),
-        ne(tasks.type, 'no-schedule')
-      )
+    const expired = await this.db
+      .select()
+      .from(tasks)
+      .where(and(lte(tasks.date, new Date()), ne(tasks.type, "no-schedule")));
+
+    console.log(`Executing ${expired.length} expired tasks`);
+
+    const results = await Promise.allSettled(
+      expired.map(async (task) => {
+        let error: Error | undefined;
+
+        // Keep the error in the result.
+        await this.onScheduledTask(task).catch((e) => {
+          error = e;
+        });
+
+        if (task.type === "cron" && task.cron) {
+          await this.db
+            .update(tasks)
+            .set({
+              date: this.getNextCronTime(task.cron),
+            })
+            .where(eq(tasks.id, task.id));
+        } else {
+          await this.db.delete(tasks).where(eq(tasks.id, task.id));
+        }
+
+        return { id: task.id, error };
+      })
     );
 
-    const results = await Promise.allSettled(expired.map(async (task) => {
-      let error: Error | undefined;
-
-      // Keep the error in the result.
-      await this.onScheduledTask(task).catch(e => {
-        error = e;
-      });
-
-      if (task.type === "cron" && task.cron) {
-        await this.db.update(tasks).set({
-          date: this.getNextCronTime(task.cron),
-        }).where(eq(tasks.id, task.id));
-      } else {
-        await this.db.delete(tasks).where(eq(tasks.id, task.id));
-      }
-
-      return { id: task.id, error };
-    }));
-
     // If any tasks threw an uncaught error - log them out here.
-    if (results.filter(r => r.status === 'rejected').length > 0) {
-      console.error('Failed to execute ${failed.length} tasks');
+    if (results.filter((r) => r.status === "rejected").length > 0) {
+      console.error(
+        `Failed to execute ${
+          results.filter((r) => r.status === "rejected").length
+        } tasks`
+      );
     }
 
     // If any tasks failed calling `onSchedule`, log them out here.
-    const failed = results.filter(r => r.status === 'fulfilled').map(r => r.value).filter(r => r.error);
+    const failed = results
+      .filter((r) => r.status === "fulfilled")
+      .map((r) => r.value)
+      .filter((r) => r.error);
     if (failed.length > 0) {
-      console.error(`Failed to execute ${failed.length} tasks\n\n: ${failed.map(f => `  ${f.id}: ${f.error}`).join('\n')}`);
+      console.error(
+        `Failed to execute ${failed.length} tasks\n\n: ${failed
+          .map((f) => `  ${f.id}: ${f.error}`)
+          .join("\n")}`
+      );
     }
 
     // Schedule the next alarm
@@ -162,59 +189,77 @@ export abstract class DurableScheduler<Env> extends DurableObject<Env> {
     const id = schedule.id || crypto.randomUUID();
     let record: Task | undefined;
 
-    if (schedule.type === 'cron') {
-      [record] = await this.db.insert(tasks).values({
-        id,
-        description: schedule.description,
-        type: schedule.type,
-        cron: schedule.cron,
-        payload: schedule.payload,
-      }).returning();
-    } else if (schedule.type === 'delayed') {
-      [record] = await this.db.insert(tasks).values({
-        id,
-        description: schedule.description,
-        type: schedule.type,
-        date: new Date(Date.now() + schedule.seconds * 1000),
-        delayInSeconds: schedule.seconds,
-        payload: schedule.payload ?? {},
-      }).returning();
-    } else if (schedule.type === 'scheduled') {
-      [record] = await this.db.insert(tasks).values({
-        id,
-        description: schedule.description,
-        type: schedule.type,
-        date: schedule.date,
-        payload: schedule.payload,
-      }).returning();
-    } else if (schedule.type === 'no-schedule') {
-      [record] = await this.db.insert(tasks).values({
-        id,
-        description: schedule.description,
-        type: schedule.type,
-        payload: schedule.payload,
-      }).returning();
+    if (schedule.type === "cron") {
+      [record] = await this.db
+        .insert(tasks)
+        .values({
+          id,
+          description: schedule.description,
+          type: schedule.type,
+          cron: schedule.cron,
+          payload: schedule.payload,
+        })
+        .returning();
+    } else if (schedule.type === "delayed") {
+      [record] = await this.db
+        .insert(tasks)
+        .values({
+          id,
+          description: schedule.description,
+          type: schedule.type,
+          date: new Date(Date.now() + schedule.seconds * 1000),
+          delayInSeconds: schedule.seconds,
+          payload: schedule.payload ?? {},
+        })
+        .returning();
+    } else if (schedule.type === "scheduled") {
+      [record] = await this.db
+        .insert(tasks)
+        .values({
+          id,
+          description: schedule.description,
+          type: schedule.type,
+          date: schedule.date,
+          payload: schedule.payload,
+        })
+        .returning();
+    } else if (schedule.type === "no-schedule") {
+      [record] = await this.db
+        .insert(tasks)
+        .values({
+          id,
+          description: schedule.description,
+          type: schedule.type,
+          payload: schedule.payload,
+        })
+        .returning();
     }
 
     if (!record) {
-      throw new Error('Failed to create task');
+      throw new Error("Failed to create task");
     }
 
     await this.scheduleNextAlarm();
+    console.log(`Created task: ${JSON.stringify(record)}`);
     return record.id;
   }
 
   // Counts the number of tasks.
-  async countTasks(query?: {
-    type?: Task['type'];
-  }) {
-    return this.db.select({ count: count() }).from(tasks).where(query?.type ? eq(tasks.type, query.type) : undefined).then(r => r[0].count);
+  async countTasks(query?: { type?: Task["type"] }) {
+    return this.db
+      .select({ count: count() })
+      .from(tasks)
+      .where(query?.type ? eq(tasks.type, query.type) : undefined)
+      .then((r) => r[0].count);
   }
 
   // Cancels a schedule by ID.
   // Returns a boolean value if the scheduled task existed.
   async cancelTask(id: string): Promise<boolean> {
-    const deleted = await this.db.delete(tasks).where(eq(tasks.id, id)).returning();
+    const deleted = await this.db
+      .delete(tasks)
+      .where(eq(tasks.id, id))
+      .returning();
     await this.scheduleNextAlarm();
     return deleted.length > 0;
   }
@@ -227,8 +272,15 @@ export abstract class DurableScheduler<Env> extends DurableObject<Env> {
 
   // Updates a task by ID.
   // Returns a boolean value if the scheduled task existed.
-  async updateTask(id: string, task: Omit<Task, 'id' | 'createdAt'>): Promise<boolean> {
-    const updated = await this.db.update(tasks).set(task).where(eq(tasks.id, id)).returning();
+  async updateTask(
+    id: string,
+    task: Omit<Task, "id" | "createdAt">
+  ): Promise<boolean> {
+    const updated = await this.db
+      .update(tasks)
+      .set(task)
+      .where(eq(tasks.id, id))
+      .returning();
     await this.scheduleNextAlarm();
     return updated.length > 0;
   }
@@ -238,8 +290,11 @@ export abstract class DurableScheduler<Env> extends DurableObject<Env> {
     return this.db.query.tasks.findMany({
       limit: query?.limit ?? 100,
       offset: query?.offset,
-      orderBy: (tasks, { asc, desc }) => [query?.orderBy === 'asc' ? asc(tasks.date) : desc(tasks.date)],
-      where: (tasks, { eq }) => query?.type ? eq(tasks.type, query.type) : undefined,
+      orderBy: (tasks, { asc, desc }) => [
+        query?.orderBy === "asc" ? asc(tasks.date) : desc(tasks.date),
+      ],
+      where: (tasks, { eq }) =>
+        query?.type ? eq(tasks.type, query.type) : undefined,
     });
   }
 
